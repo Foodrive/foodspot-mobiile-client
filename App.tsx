ff457bedfe01eb08915d44d1360a93e4cf297ed1
Chris Rabe <chris.rabe14@gmail.com>
2021-07-17 06:40:52 +1000

    added loading control

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
 import AppProvider from "@app/components/common/AppProvider";
 import RootStack from "@app/navigation";
 
-export default function App() {
+const App: React.FC = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  const getCachedResources = useCallback(async () => {
+    // Load cached assets here
+    // Can use expo-asset for cache control
+  }, []);
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={getCachedResources}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <AppProvider>
       <RootStack />
       <StatusBar style="auto" hidden />
     </AppProvider>
   );
-}
+};
+
+export default App;
