921227f2f1bbb0852cc47e95a6a7600b8c2126ec
tomas.lawton <tomas.lawton@accenture.com>
2021-07-19 16:15:13 +1000

    worlds worst button

--- src/screens/Events/index.tsx
+++ src/screens/Events/index.tsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, Text } from "react-native";
+import { Button } from 'react-native-elements';
 import styles from "./styles";
+import { gql, useMutation } from '@apollo/client';
 
-const UserEvents: React.FC = () => (
+const UserEvents: React.FC = () => {
+const UserId = "owbngeobweogbwoieb"
+const mockLocation = {
+   address: "Food Shelter",
+   coords: {
+     longitude: 1938986135,
+     latitude: 1938986246
+   }
+  }
+
+const mockFood = [{name: "Pizza", description: "nom noms", servings: 5, allergens: []}]
+
+const CREATE_REQUEST = gql`
+mutation Mutation($createRequestRequestorId: ID!, $createRequestLocation: LocationInput!, $createRequestNumAttendees: Int!, $createRequestFood: [FoodInput!]!) {
+  createRequest(requestorId: $createRequestRequestorId, location: $createRequestLocation, numAttendees: $createRequestNumAttendees, food: $createRequestFood) {
+    numAttendees
+    food {
+      name
+      description
+      servings
+      allergens
+    }
+  }
+}
+`;
+
+const [claimFood, { data }] = useMutation(CREATE_REQUEST);
+const [isClaimed, updateClaim] = useState(false)
+
+return (
   <View style={styles.container}>
     <Text style={styles.text}>Event Screen</Text>
+
+    <View style={styles.buttonContainer}>
+      <Button
+        title={isClaimed ? "Unclaim" : "Claim"}
+        onPress={() => {
+          updateClaim(oldState => !oldState)
+          claimFood({ variables: {
+            requestorId: UserId,
+            location: mockLocation,
+            numAttendees: 4,
+            food: mockFood
+            }
+          });
+        }}
+        buttonStyle={isClaimed ? styles.buttonStyleClaimed : styles.buttonStyleUnclaimed}
+      />
+    </View>
   </View>
-);
+)};
 
 export default UserEvents;
--- src/screens/Events/styles.ts
+++ src/screens/Events/styles.ts
@@ -6,11 +6,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    margin: 20,
   },
   text: {
     fontSize: RFValue(24),
     textAlign: "center",
   },
+  buttonContainer: {
+    padding: 0,
+    margin: 0,
+    marginTop: 30,
+    width: "100%",
+  },
+  buttonStyleClaimed: {
+    borderRadius: 5,
+    width: "100%",
+    backgroundColor: '#382C1E'
+  },
+  buttonStyleUnclaimed: {
+      borderRadius: 5,
+      width: "100%",
+      backgroundColor: '#59BBD0'
+  }
 });
 
 export default styles;
