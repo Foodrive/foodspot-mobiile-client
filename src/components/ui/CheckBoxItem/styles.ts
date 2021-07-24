bd15533f5a3bdf03767e78bc37de29cf1eb9531a
Chris Rabe <chris.rabe14@gmail.com>
2021-07-24 10:00:45 +1000

    fix: text style

---
+++ src/components/ui/CheckBoxItem/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { montserrat } from "@app/utils";
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontWeight: "300",
+    fontFamily: montserrat.regular,
+  },
+});
+
+export default styles;
