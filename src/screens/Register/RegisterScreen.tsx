0a3e0b306ac9c5ddcefb5e389ce8eabe0c15be83
Chris Rabe <chris.rabe14@gmail.com>
2021-07-28 21:02:10 +1000

    feat: binded form input to react hook form. added logout button

--- src/screens/Register/RegisterScreen.tsx
+++ src/screens/Register/RegisterScreen.tsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { ImageBackground, ScrollView, Text, View } from "react-native";
 import { useStyles } from "./styles";
 import Card from "@app/components/ui/Card";
-import { FormInput } from "@app/components/forms";
+import { FormInput, getErrorMessage } from "@app/components/forms";
 import { useForm } from "react-hook-form";
 import Button from "@app/components/ui/Button";
 import { IconButton } from "@app/components/ui";
 import { useNavigation } from "@react-navigation/native";
+import SCREEN_NAMES from "@app/navigation/screen.names";
 
 const RegisterScreen: React.FC = () => {
-  const { control } = useForm();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const styles = useStyles();
   const navigation = useNavigation();
+
+  const onSubmit = useCallback(
+    (data) => {
+      navigation.navigate(SCREEN_NAMES.common.app, {
+        screen: SCREEN_NAMES.app.home,
+      });
+    },
+    [navigation],
+  );
 
   return (
     <View style={styles.container}>
@@ -36,6 +50,8 @@ const RegisterScreen: React.FC = () => {
               type="user"
               autoFocus
               placeholder="Enter username"
+              rules={{ required: true }}
+              errorMessage={getErrorMessage("Username", errors.username)}
             />
             <FormInput
               name="firstName"
@@ -44,6 +60,8 @@ const RegisterScreen: React.FC = () => {
               type="user"
               autoFocus
               placeholder="Enter first name"
+              rules={{ required: true }}
+              errorMessage={getErrorMessage("First name", errors.firstName)}
             />
             <FormInput
               name="lastName"
@@ -52,6 +70,8 @@ const RegisterScreen: React.FC = () => {
               type="user"
               autoFocus
               placeholder="Enter last name"
+              rules={{ required: true }}
+              errorMessage={getErrorMessage("Last name", errors.lastName)}
             />
             <FormInput
               name="password"
@@ -60,8 +80,15 @@ const RegisterScreen: React.FC = () => {
               type="password"
               autoFocus
               placeholder="Enter password"
+              rules={{ required: true }}
+              errorMessage={getErrorMessage("Password", errors.password)}
             />
-            <Button id="confirm-button" title="Sign up" color="primary" />
+            <Button
+              id="confirm-button"
+              title="Sign up"
+              color="primary"
+              onPress={handleSubmit(onSubmit)}
+            />
           </ScrollView>
         </Card>
       </ImageBackground>
