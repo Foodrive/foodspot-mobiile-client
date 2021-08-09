f940c4cf0b05673b83b8e38d197cb2210eb27c37
Andrew <a.a.chan@accenture.com>
2021-08-10 00:48:35 +1000

    feat: event details page

--- src/screens/Events/EventDetails/EventDetails.tsx
+++ src/screens/Events/EventDetails/EventDetails.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "@apollo/client";
 import { GET_FOOD_DRIVE_BY_ID } from "@app/graphql/queries/events.queries";
 import { IconButton } from "@app/components/ui";
 import styles from "./styles";
+import EventDetailsCard from "./EventDetailsCard";
+import Button from "@app/components/ui/Button";
 
 type EventDetailsProps = EventDetailsPropsFromRedux; // interface EventDetailsProps extends EventDetailsPropsFromRedux when needed
 
@@ -19,8 +21,8 @@ const EventDetails: React.FC<EventDetailsProps> = ({ eventId }) => {
 
   // TODO
   return (
-    <View>
-      <View style={styles.backButton}>
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
         <IconButton
           id="back-icon-button"
           icon="chevron-back-outline"
@@ -28,33 +30,42 @@ const EventDetails: React.FC<EventDetailsProps> = ({ eventId }) => {
             navigation.goBack();
           }}
         />
+        <Text style={styles.heading}>Event Details</Text>
+        <View style={styles.floatingIconButton}>
+          <IconButton
+            id="share-icon-button"
+            icon="share-social-outline"
+            onPress={() => {
+              console.log("share"); // TODO
+            }}
+            reverse={false}
+          />
+        </View>
       </View>
       <View>
-        <Text style={styles.text}>Event Details</Text>
         {!loading && data.getFoodDriveById && (
           <>
-            <Text style={styles.text}>Name: {data.getFoodDriveById.name}</Text>
-            <Text style={styles.text}>
-              Description: {data.getFoodDriveById.description}
-            </Text>
-            <Text style={styles.text}>
-              Start Date: {data.getFoodDriveById.startDate}
-            </Text>
-            <Text style={styles.text}>
-              End Date: {data.getFoodDriveById.endDate}
-            </Text>
-            <Text style={styles.text}>
-              Address: {data.getFoodDriveById.location.address}
-            </Text>
-            <Text style={styles.text}>
-              Contact Number: {data.getFoodDriveById.contactNumber}
-            </Text>
-            <Text style={styles.text}>
-              Allergens:{" "}
-              {data.getFoodDriveById.food
-                .map((food) => food.allergens.join(", "))
-                .join(", ")}
-            </Text>
+            <EventDetailsCard
+              name={data.getFoodDriveById.name}
+              startDate={data.getFoodDriveById.startDate}
+              endDate={data.getFoodDriveById.endDate}
+              description={data.getFoodDriveById.description}
+              address={data.getFoodDriveById.location.address}
+              contactNumber={data.getFoodDriveById.contactNumber}
+              allergens={data.getFoodDriveById.food.reduce(
+                (accm: string[], food: { allergens: string[] }) => {
+                  accm = accm.concat(food.allergens);
+                  return accm;
+                },
+                [],
+              )}
+            />
+            <Button
+              id="next-button"
+              title="Next"
+              onPress={() => console.log("next")} // TODO
+              titleStyle={styles.buttonTitle}
+            />
           </>
         )}
       </View>
