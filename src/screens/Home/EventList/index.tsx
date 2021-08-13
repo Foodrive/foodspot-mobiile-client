967a1ca674b1d0686a724cad97c74c623d5e3847
Chris Rabe <chris.rabe14@gmail.com>
2021-08-13 16:20:11 +1000

    fix(pr): fixed pull request issues

--- src/screens/Home/EventList/index.tsx
+++ src/screens/Home/EventList/index.tsx
@@ -1,45 +1,110 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { USER_FOOD_DRIVES } from "@app/graphql/queries";
 import { useQuery } from "@apollo/client";
-import { Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import ListItem from "@app/components/ui/ListItem";
-import { format } from "date-fns";
-import styles from "./styles";
+import dayjs from "dayjs";
+import useToastProvider from "@app/hooks/useToastProvider";
+import { useStyles } from "./styles";
+import { colors } from "@app/utils";
+
+interface DayInfo {
+  dayText: string;
+  timeText: string;
+  isToday: boolean;
+}
 
 const EventList: React.FC = () => {
   const { loading, error, data } = useQuery(USER_FOOD_DRIVES);
+  const toastProvider = useToastProvider();
+  const styles = useStyles();
 
-  if (loading) return <Text>Loading...</Text>;
-  if (error) return <Text>Error :(</Text>;
+  const getDateInfo = useCallback(
+    (startDate: string, endDate: string): DayInfo => {
+      const now = dayjs();
+      const start = dayjs(startDate);
+      const end = dayjs(endDate);
 
-  const getUserDate = (time: any): string => {
-    return format(new Date(time), "EEEE dd MMMM");
-  };
+      const startTime = start.format("hh:mm a");
+      const endTime = end.format("hh:mm a");
 
-  const getUserTime = (time: any): string => {
-    return format(new Date(time), "h:mm a");
-  };
+      const result: DayInfo = {
+        dayText: "",
+        timeText: "",
+        isToday: false,
+      };
+
+      if (start.isSame(end, "day")) {
+        if (start.isSame(now, "day")) {
+          result.isToday = true;
+          result.dayText = "Today";
+        } else if (start.isSame(now, "week")) {
+          result.dayText = start.format("ddd");
+        } else {
+          result.dayText = start.format("DD MMMM");
+        }
+        result.timeText = `${startTime} - ${endTime}`;
+      } else {
+        let startDay = start.format("DD MMMM");
+        const endDay = end.format("DD MMMM");
+        if (start.isSame(now, "day")) {
+          startDay = "Today";
+          result.isToday = true;
+        }
+        result.dayText = `${startDay} ${startTime} -`;
+        result.timeText = `${endDay} ${endTime}`;
+      }
+      return result;
+    },
+    [],
+  );
+
+  useEffect(() => {
+    if (error) {
+      toastProvider.showError(error.message);
+    }
+  }, [error, toastProvider]);
 
   const handleEventSelection = useCallback(() => {
     //   do somethin
   }, []);
 
-  return data.getFoodDrives.map((listItem: any, index: number) => (
-    <ListItem
-      key={`event-list-item-${index}`}
-      id={`event-list-item-${index}`}
-      title={listItem.name}
-      otherInformation={
-        <>
-          <Text style={styles.boldDate}>
-            {getUserDate(listItem.startDate)},
-          </Text>
-          <Text> {getUserTime(listItem.startDate)}</Text>
-        </>
-      }
-      onClick={handleEventSelection}
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <></>;
+  }
+
+  const renderItem = ({ item }: any) => {
+    const dateInfo = getDateInfo(item.startDate, item.endDate);
+    const color = dateInfo.isToday ? colors.success : colors.lightbrown;
+    return (
+      <ListItem
+        iconColor={color}
+        key={`event-list-item-${item.id}`}
+        id={`event-list-item-${item.id}`}
+        style={{ marginVertical: 5 }}
+        title={item.name}
+        otherInformation={
+          <View>
+            <Text style={[styles.dateText, { color }]}>{dateInfo.dayText}</Text>
+            <Text style={styles.timeText}>{dateInfo.timeText}</Text>
+          </View>
+        }
+        onPress={handleEventSelection}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      data={data.getFoodDrives}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
     />
-  ));
+  );
 };
 
 export default EventList;
--- src/screens/Home/EventList/styles.ts
+++ src/screens/Home/EventList/styles.ts
@@ -1,12 +1,13 @@
 import { StyleSheet } from "react-native";
-import { RFValue } from "react-native-responsive-fontsize";
+import { colors, montserrat } from "@app/utils";
 
-const styles = StyleSheet.create({
-  boldDate: {
-    fontSize: RFValue(12),
-    fontWeight: "bold",
-    marginRight: RFValue(5),
-  },
-});
-
-export default styles;
+export const useStyles = () =>
+  StyleSheet.create({
+    dateText: {
+      fontFamily: montserrat.bold,
+    },
+    timeText: {
+      fontFamily: montserrat.regular,
+      color: colors.darkAccent,
+    },
+  });
