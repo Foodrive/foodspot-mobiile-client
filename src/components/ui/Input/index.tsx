e71f53c46b497ba2eb41fb94216105823ab9a5dc
Andrew Truong <andrew.b.truong@accenture.com>
2021-08-25 08:19:45 +1000

    fixed conflict

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -49,13 +49,8 @@ const Input: React.FC<InputProps> = (props) => {
       data-testid={id}
       leftIcon={icon}
       keyboardType={keyboard}
-<<<<<<< HEAD
       inputContainerStyle={styles.inputContainer}
       inputStyle={editable ? styles.input : styles.uneditableInput}
-=======
-      inputContainerStyle={[styles.inputContainer, inputContainerStyle]}
-      inputStyle={[styles.input, inputStyle]}
->>>>>>> develop
       secureTextEntry={type === "password"}
       disabled={!editable}
 
