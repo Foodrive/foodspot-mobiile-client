36c8474a06e334089c8901369e1e131ac40397ca
Chris Rabe <chris.rabe14@gmail.com>
2021-08-26 19:09:08 +1000

    fix: moved event create data to sliice

--- src/redux/slices/events.slice.ts
+++ src/redux/slices/events.slice.ts
@@ -1,5 +1,24 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { EventCreateData, EventType } from "@app/types/event.types";
+import { EventType } from "@app/types/event.types";
+
+interface EventCreateData {
+  // Basic details
+  type: EventType;
+  location?: string; // TODO extend this to be location type
+  startDate?: string;
+  endDate?: string;
+  // Event description
+  name?: string;
+  description?: string;
+  allergens?: string[]; // This is converted to food item
+  // Planning details
+  maxCapacity?: number;
+  // TODO extend to support number of volunteers
+  // Contact details
+  contactNumber?: string;
+  email?: string;
+  // TODO extend to support facebook page
+}
 
 // Payload Types
 type CreateDataChange = Partial<EventCreateData>;
--- src/types/event.types.ts
+++ src/types/event.types.ts
@@ -1,22 +1,3 @@
 export enum EventType {
   foodDrive = "FoodDrive",
 }
-
-export interface EventCreateData {
-  // Basic details
-  type: EventType;
-  location?: string; // TODO extend this to be location type
-  startDate?: string;
-  endDate?: string;
-  // Event description
-  name?: string;
-  description?: string;
-  allergens?: string[]; // This is converted to food item
-  // Planning details
-  maxCapacity?: number;
-  // TODO extend to support number of volunteers
-  // Contact details
-  contactNumber?: string;
-  email?: string;
-  // TODO extend to support facebook page
-}
