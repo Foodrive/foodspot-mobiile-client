bfabfd4d93a331376848a31df82b1277ebfe90c8
Andrew Truong <andrew.b.truong@accenture.com>
2021-09-08 20:11:52 +1000

    added updateUser details functionality

--- src/screens/Profile/UserProfile.tsx
+++ src/screens/Profile/UserProfile.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect, useMemo } from "react";
 import { View, Text, ScrollView } from "react-native";
 import Input from "@app/components/ui/Input";
+import { FormInput, FormCheckSelect ,getErrorMessage } from "@app/components/forms";
+import { useForm } from "react-hook-form";
 import CheckboxItem from "@app/components/ui/CheckBoxItem";
 import IconButton from "@app/components/ui/IconButton";
 import styles from "./styles";
@@ -10,24 +12,41 @@ import SCREEN_NAMES from "@app/navigation/screen.names";
 import SecureStore, { SecureStoreEnum } from "@app/services/secure.store";
 import { Divider } from "react-native-elements";
 import { Allergies } from "../../utils/constants";
+import { allergens } from "@app/utils/constants";
 import { ProfilePropsFromRedux } from "./container";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { GET_USER } from "@app/graphql/queries/user.queries";
+import { UPDATE_USER } from "@app/graphql/mutations/user.mutation";
 import useToastProvider from "@app/hooks/useToastProvider";
+import { regexValidator } from "@app/utils/validators";
 
 type ProfileProps = ProfilePropsFromRedux;
 
 const UserProfile: React.FC<ProfileProps> = ({
-  currentUser
+  currentUser,
 }) => {
 
   console.log("current user:", currentUser)
 
-  const { error, loading, data} = useQuery(GET_USER, {
+  const [updateUser, { error, loading, data: gqlData }] = useMutation(UPDATE_USER);
+
+  const { queryError, queryLoading, data : queryData} = useQuery(GET_USER, {
     variables: {
       getUserUsername: currentUser?.username
     }
   });
+
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const allergenOptions = useMemo(
+    () => allergens.map((item) => ({ displayText: item, value: item })),
+    [],
+  );
   const [user, setUser] = useState();
   const toastProvider = useToastProvider();
   const navigation = useNavigation();
@@ -50,20 +69,40 @@ const UserProfile: React.FC<ProfileProps> = ({
     console.log(updatedCheckedState);
   };
 
+  const onSubmit = useCallback(
+    async (data) => {
+      try {
+        await updateUser({
+          variables: {
+            password: data.password,
+            phoneNumber: data.contactNumber,
+            email: data.email,
+            allergies: data.allergies,
+            firstName: data.firstName,
+            lastName: data.lastName
+          }
+        })
+      } catch (e) {
+        // Do nothing
+      }
+    },
+    [updateUser]
+  );
+  
   const handleEditIconPress = () => {
     setEditable(!editable);
   };
 
   useEffect(() => {
     console.log("use effect triggered");
-    console.log(data);  //TO DO why is this undefined?
-    if (data && !loading && !error) {
-      setUser(data);
-    } else if (error){
+    console.log(queryData);  //TO DO why is this undefined?
+    if (queryData && !queryLoading && !queryError) {
+      setUser(queryData);
+    } else if (queryError){
       console.log("error found!")
-      toastProvider.showError(error.message);
+      toastProvider.showError(queryError.message);
     }
-  }, [data]);
+  }, [queryData]);
 
   return (
     <ScrollView style={styles.container}>
@@ -76,17 +115,42 @@ const UserProfile: React.FC<ProfileProps> = ({
       </View>
       <Text style={styles.title}>User Settings</Text>
       <View style={styles.editButton}>
-        <IconButton icon="create-outline" reverse={false} onPress={handleEditIconPress} color={editable ? "primary" : "default"}  /> 
+        <IconButton icon="create-outline" reverse={false} onPress={handleSubmit(onSubmit)} color={editable ? "primary" : "default"}  /> 
       </View>
       <View style={styles.profileCard}>
         <Text style={styles.profileTitle}>Your Profile</Text>
         <Divider orientation="horizontal" width={1} color={"#d9d9d9"} style={styles.divider} />   
-        <Text style={styles.inputTitle}>Name</Text>
-        <Input id="user-profile-input-name" type="user" editable={editable}>{user?.getUser.firstName + " " + user?.getUser.lastName}</Input>
-        <Text style={styles.inputTitle}>Contact Number</Text>
-        <Input id="user-profile-number-name" type="number" editable={editable}>{user?.getUser.phoneNumber}</Input>
-        <Text style={styles.inputTitle}>Email</Text>
-        <Input id="user-profile-email-name" type="email" editable={editable}>{user?.getUser.email}</Input>
+        <FormInput 
+          name="name"
+          label="Name"
+          control={control}
+          type="user"
+          autoFocus
+          placeholder={user?.getUser.firstName + " " + user?.getUser.lastName}
+          rules={{required: true}}
+          errorMessage={getErrorMessage("Name", errors.name)}
+          // defaultValue={user?.getUser.firstName + user?.getUser.lastName}
+          // value={user?.getUser.firstName + " " + user?.getUser.lastName}
+        />
+        <FormInput 
+          name="contactNumber"
+          label="Contact Number"
+          control={control}
+          type="number"
+          autoFocus
+          placeholder="Enter Contact Number"
+          errorMessage={getErrorMessage("Contact Number", errors.contactNumber)}
+        />
+        <FormInput 
+          name="email"
+          label="Email"
+          control={control}
+          type="email"
+          autoFocus
+          placeholder="Enter Email"
+          rules={{required: true, pattern: regexValidator.email}}
+          errorMessage={getErrorMessage("Email", errors.email)}
+        />
         <Text style={styles.inputTitle}>Allergies</Text>
         <View style={styles.allergyList}>
           {Allergies.map(({name}, index) => {
@@ -95,6 +159,14 @@ const UserProfile: React.FC<ProfileProps> = ({
             );
           })}
         </View>
+        <Button 
+          id="save-button"
+          title="Save"
+          color="secondary"
+          onPress={handleSubmit(onSubmit)}
+          loading={loading}
+          disabled={loading}
+        />
         <Button id="logout-button" title="Log out" onPress={handleLogout} /> 
       </View>
     </ScrollView>
