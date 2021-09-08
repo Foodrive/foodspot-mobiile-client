b74c7f2298d86d8bfa99eb175160b9eebcc4e0e0
Andrew Truong <andrew.b.truong@accenture.com>
2021-09-08 20:10:44 +1000

    updated UserInfo type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,9 @@
 export interface UserInfo {
   username: string;
   id: string;
+  firstName?: string;
+  lastName?: string;
+  contactNumber?: string;
+  email?: string;
+  allergens?: string[];  
 }
