3f29879d51456a7c9f61c73a8d1da10505e39506
Chris Rabe <chris.rabe14@gmail.com>
2021-10-02 21:11:56 +1000

    fix: fixed date text and icon colour

--- src/screens/Events/YourEvents.tsx
+++ src/screens/Events/YourEvents.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { useQuery } from "@apollo/client";
 import {
   GetUserInvitation,
@@ -9,10 +9,9 @@ import {
 import { EventsPropsFromRedux } from "./container";
 import { useNavigation } from "@react-navigation/native";
 import { ListItem, SectionedList } from "@app/components/ui";
-import { colors, deepClone } from "@app/utils";
+import { colors, deepClone, getDateInfo } from "@app/utils";
 import { InvitationStatus } from "@app/utils/constants";
 import SCREEN_NAMES from "@app/navigation/screen.names";
-import dayjs from "dayjs";
 import styles from "./styles";
 import useToastProvider from "@app/hooks/useToastProvider";
 import { SectionedListData } from "@app/components/ui/SectionedList";
@@ -72,26 +71,36 @@ const YourEvents: React.FC<EventsPropsFromRedux> = ({
     }
   }, [data, loading, error]);
 
+  const renderItem = (invitation: UserInvitation) => {
+    const { event } = invitation;
+    const dateInfo = getDateInfo(event.startDate, event.endDate);
+    const color = dateInfo.isToday ? colors.success : colors.lightbrown;
+    return (
+      <ListItem
+        id={`${invitation.id}-list-item`}
+        title={invitation.event.name}
+        otherInformation={
+          <View>
+            <Text style={[styles.dateText, { color }]}>{dateInfo.dayText}</Text>
+            <Text style={styles.timeText}>{dateInfo.timeText}</Text>
+          </View>
+        }
+        onPress={() => {
+          setCurrentEventId(invitation.event.id);
+          setCurrentInvitationId(invitation.id);
+          navigation.navigate(SCREEN_NAMES.common.events.eventDetails);
+        }}
+        iconColor={color}
+      />
+    );
+  };
+
   return (
     <View style={styles.eventList}>
       <SectionedList
         id="sectioned-list"
         data={invitations}
-        renderItem={(invitation) => (
-          <ListItem
-            id={`${invitation.id}-list-item`}
-            title={invitation.event.name}
-            otherInformation={`${dayjs(invitation.event.startDate).format(
-              "DD MMM YY h:mm A",
-            )} - ${dayjs(invitation.event.endDate).format("DD MMM YY h:mm A")}`}
-            onPress={() => {
-              setCurrentEventId(invitation.event.id);
-              setCurrentInvitationId(invitation.id);
-              navigation.navigate(SCREEN_NAMES.common.events.eventDetails);
-            }}
-            iconColor={colors.success}
-          />
-        )}
+        renderItem={renderItem}
       />
     </View>
   );
